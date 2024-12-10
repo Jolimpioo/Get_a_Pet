47691a6bd0c93d19f21e0f9584af3583898be150
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-10 16:49:48 -0300

    Salvando 'pet' no banco de dados

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import UserController from "../controllers/userController.js";
+import UserController from "../controllers/UserController.js";
 
 // middlewares
 import verifyToken from "../helpers/verify-token.js";
