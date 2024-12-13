0eade74e03550e7a5d9f39429d52f10ce95f5109
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-13 16:25:46 -0300

    Atualização de Pet

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -133,6 +133,7 @@ class PetController {
 
     if (!pet) {
       res.status(404).json({ message: "Pet não encontrado!" });
+      return;
     }
 
     // check if logger user registered the pet
@@ -142,11 +143,86 @@ class PetController {
     // validate ownership
     if (pet.user._id.toString() !== user._id.toString()) {
       res.status(422).json({
-        message: "Você não tem permissão para remover este pet!",
+        message:
+          "Houve um problema em processar a sua solicitação, tente novamente mais tarde!",
       });
+      return;
     }
     await Pet.findByIdAndDelete(id);
     res.status(200).json({ message: "Pet removido com sucesso!" });
+  }
+
+  static async updatePet(req, res) {
+    const id = req.params.id;
+
+    const { name, age, weight, color, available } = req.body;
+
+    const images = req.files;
+
+    const updatedData = {};
+
+    // check if pet exists
+    const pet = await Pet.findById(id);
+
+    if (!pet) {
+      res.status(404).json({ message: "Pet não encontrado!" });
+      return;
+    }
+
+    // check if logger user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    // validate ownership
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message:
+          "Houve um problema em processar a sua solicitação, tente novamente mais tarde!",
+      });
+      return;
+    }
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    } else {
+      updatedData.name = name;
+    }
+
+    if (!age) {
+      res.status(422).json({ message: "A idade é obrigatória!" });
+      return;
+    } else {
+      updatedData.age = age;
+    }
+
+    if (!weight) {
+      res.status(422).json({ message: "O peso é obrigatório!" });
+      return;
+    } else {
+      updatedData.weight = weight;
+    }
+
+    if (!color) {
+      res.status(422).json({ message: "A cor é obrigatória!" });
+      return;
+    } else {
+      updatedData.color = color;
+    }
+
+    if (images.length == 0) {
+      res.status(422).json({ message: "A imagem é obrigatória!" });
+    } else {
+      updatedData.images = [];
+      images.map((image) => {
+        updatedData.images.push(image.filename);
+      });
+    }
+
+    await Pet.findByIdAndUpdate(id, updatedData);
+
+    res.status(200).json({ message: "Pet atualizado com sucesso!" });
   }
 }
 
