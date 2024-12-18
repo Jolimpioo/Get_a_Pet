b17dfff35a1e1279e3a94f4343c0f445aab0b194
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-17 23:32:27 -0300

    Ajustes nas configurações frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/* pages */
+import Login from "./components/pages/Auth/Login.js";
+import Register from "./components/pages/Auth/Register.js";
+import Home from "./components/pages/Home.js";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
