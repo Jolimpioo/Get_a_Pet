1f81fef5fdf88f95e412d56c2ee8e0fbcf57bec8
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-19 16:56:14 -0300

    Criando contexto de usuario

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -3,7 +3,7 @@ import api from "../utils/api.js";
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default function useAtugh() {
+export default function useAuth() {
   async function register(user) {
     try {
       const data = await api.post("/user/register", user).then((response) => {
