96a29001b189f419b38317c6e386c8be3d6f4d00
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-26 16:12:18 -0300

    Criado componente de Flash Message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Container from "./components/layout/Container.js";
 import Login from "./components/pages/Auth/Login.js";
 import Register from "./components/pages/Auth/Register.js";
 import Home from "./components/pages/Home.js";
+import Message from "./components/layout/Message.js";
 
 /* context */
 import { UserProvider } from "./context/UserContext.js";
@@ -18,6 +19,7 @@ function App() {
     <Router>
       <UserProvider>
         <Navbar />
+        <Message />
         <Container>
           <Routes>
             <Route path="/login" element={<Login />} />
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,17 +2,26 @@ import api from "../utils/api.js";
 
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useFlashMessage from "./useFlashMessage.js";
 
 export default function useAuth() {
+  const { setFlashMessage } = useFlashMessage();
+
   async function register(user) {
+    let msgText = "Cadastro realizado com sucesso!";
+    let msgType = "success";
+
     try {
       const data = await api.post("/users/register", user).then((response) => {
         return response.data;
       });
       console.log(data);
     } catch (error) {
-      console.log(error);
+      msgText = error.response.data.message;
+      msgType = "error";
     }
+
+    setFlashMessage(msgText, msgType);
   }
   return { register };
 }
