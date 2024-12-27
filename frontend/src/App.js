534a5c3cfbfa081198ca088034f7527c0ef28c03
Jolimpioo <jolimpiomeloj@gmail.com>
2024-12-27 16:50:16 -0300

    criando pagina de perfil do usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar.js";
 import Footer from "./components/layout/Footer.js";
 import Container from "./components/layout/Container.js";
+import Message from "./components/layout/Message.js";
 
 /* pages */
 import Login from "./components/pages/Auth/Login.js";
 import Register from "./components/pages/Auth/Register.js";
 import Home from "./components/pages/Home.js";
-import Message from "./components/layout/Message.js";
+import Profile from "./components/pages/User/Profile.js";
 
 /* context */
 import { UserProvider } from "./context/UserContext.js";
@@ -24,6 +25,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/user/profile" element={<Profile />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Container>
