723d421aba5014c3e8b0b896250ef5f7429d43dd
Jolimpioo <jolimpiomeloj@gmail.com>
2025-01-06 14:59:00 -0300

    Criando pagina de lista de pets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Login from "./components/pages/Auth/Login.js";
 import Register from "./components/pages/Auth/Register.js";
 import Home from "./components/pages/Home.js";
 import Profile from "./components/pages/User/Profile.js";
+import MyPets from "./components/pages/Pet/MyPets.js";
 
 /* context */
 import { UserProvider } from "./context/UserContext.js";
@@ -26,6 +27,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/user/profile" element={<Profile />} />
+            <Route path="/user/mypets" element={<MyPets />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Container>
